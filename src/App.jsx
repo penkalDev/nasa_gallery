fca8d020b718899560228f3f7989d1c6b39386d1
penkalDev <penkaldev@gmail.com>
2025-09-30 21:52:37 +0200

    Api key added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from "react";
 import axios from "axios";
 import SearchInput from "./components/SearchInput";
@@ -8,18 +7,22 @@ import Header from "./components/Header";
 function App() {
   const [images, setImages] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [headerImageUrl, setHeaderImageUrl] = useState('');
-  const [headerImageTitle, setHeaderImageTitle] = useState('') // Stan do przechowywania URL obrazu nagłówka
+  const [headerImageUrl, setHeaderImageUrl] = useState("");
+  const [headerImageTitle, setHeaderImageTitle] = useState(""); // Stan do przechowywania URL obrazu nagłówka
   const apiUrl = "https://images-api.nasa.gov/search";
+  const apiKey = import.meta.env.VITE_NASA_KEY;
 
   // Funkcja do pobierania obrazu dnia z NASA
   const fetchHeaderImage = async () => {
     try {
-      const response = await axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
-      console.log(response.data.title);
-    
-      setHeaderImageUrl(response.data.url);// Ustawia URL obrazu
+      const response = await axios.get(
+        `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+      );
+
+      setHeaderImageUrl(response.data.url); // Ustawia URL obrazu
       setHeaderImageTitle(response.data.title);
+      console.log("APOD data:", response.data);
+      console.log("Title:", response.data.title);
     } catch (error) {
       console.error("Error fetching header image:", error);
     }
@@ -43,22 +46,25 @@ function App() {
 
       const response = await axios.get(apiUrl, { params });
       if (response.status !== 200) {
-        throw new Error(`Network response was not ok (status ${response.status})`);
+        throw new Error(
+          `Network response was not ok (status ${response.status})`
+        );
       }
 
       const data = response.data;
       setImages(data.collection.items);
-      setSearchTerm(searchTerm); 
+      setSearchTerm(searchTerm);
     } catch (error) {
       console.error("Error fetching data:", error);
       setImages([]);
-      setSearchTerm(searchTerm); 
+      setSearchTerm(searchTerm);
     }
   };
 
   return (
     <div>
-      <Header imageUrl={headerImageUrl} imageTitle = {headerImageTitle}/> {/* Przekazanie URL obrazu do Header */}
+      <Header imageUrl={headerImageUrl} imageTitle={headerImageTitle} />{" "}
+      {/* Przekazanie URL obrazu do Header */}
       <SearchInput onSearch={fetchNASAImages} />
       {images.length === 0 && searchTerm !== "" && (
         <h1>No results for phrase `{searchTerm}`</h1>
