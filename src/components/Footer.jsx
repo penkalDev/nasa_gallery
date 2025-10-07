0c420de46ce6a6c418a611fbfca2e3f306469609
penkalDev <penkaldev@gmail.com>
2025-10-07 10:53:01 +0200

    Small changes in ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className=" text-gray-400 text-center text-sm py-6 mt-8 border-t border-gray-800">
+  <footer className=" text-gray-300 text-center text-sm py-6 mt-8 border-t border-gray-800">
     <p>
       © {new Date().getFullYear()} — Created by <span className="text-white font-semibold">SNK</span>.
       This is a personal, non-commercial hobby project inspired by NASA’s Astronomy Picture of the Day.
--- src/components/MainAPOD.jsx
+++ src/components/MainAPOD.jsx
@@ -14,11 +14,9 @@ function MainAPOD({ data }) {
     .replace(/\./g, "-"); // tylko zamiana kropek na myślniki
 
   return (
-    
     <div className="w-5/6 bg-gray-600 p-6 rounded-2xl shadow-lg text-white flex flex-col sm:flex-row gap-8">
-      
       {/* Lewy div: obraz/video */}
-      <div className="sm:w-1/2 flex justify-center">
+      <div className="sm:w-2/3 flex justify-center">
         {data.media_type === "image" ? (
           <img
             src={data.url}
@@ -40,9 +38,13 @@ function MainAPOD({ data }) {
         <h2 className="text-2xl sm:text-3xl font-semibold mb-2">
           {data.title}
         </h2>
-        <p className="text-md text-gray-300 mb-4">Originally published:{formattedDate}</p>
-        <p className="text-gray-300 text-sm sm:text-base p-3">{data.explanation}</p>
-        <p className="text-right p-4">Author:{data.copyright}</p>
+        <p className="text-md text-gray-200 mb-4">
+          Originally published:{formattedDate}
+        </p>
+        <p className="text-gray-200 text-sm sm:text-base p-3">
+          {data.explanation}
+        </p>
+        <p className="text-right p-4">Author: {data?.copyright || "Unknown"}</p>
       </div>
     </div>
   );
