14d85346cbbd4556d787cbe15b433deefb0f5e2c
penkalDev <penkaldev@gmail.com>
2025-10-07 18:54:07 +0200

    Main text color changed to green

--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -1,6 +1,6 @@
 const Disclaimer = () => (
   <div className="w-5/6 bg-gray-500 p-6 text-center  rounded-2xl mt-4 font-medium">
-    <h1 className="text-3xl mb-3 ">Archival Astronomy Picture of the Day </h1>
+    <h1 className="text-3xl font-semibold text-green-300 mb-3 ">Archival Astronomy Picture of the Day </h1>
     <p>
       <strong>Notice:</strong> The official NASA Astronomy Picture of the Day (APOD) is no longer updated.
       This site provides an independent alternative using archival APOD images in random order.
